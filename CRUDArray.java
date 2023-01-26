ddd6460b2a6a326698ecfc49c8ce2e3e7975acd9
Rafa Campoamor <rafacampoamor@gmail.com>
2023-01-26 18:34:19 +0100

    Update CRUDArray.java

--- CRUDArray.java
+++ CRUDArray.java
@@ -4,23 +4,52 @@ public class CRUDArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int selector=0;
+		int selector = 0;
+		int contador = -1;
+		boolean encontrado = false;
 		String[] titulo = new String[20];
 		String[] descripcion = new String[20];
-		Date[] fechalimite = new Date[20];
-		
+		Date[] fechaLimite = new Date[20];
+		String nuevoTitulo = new String();
+		String nuevaDescripcion = new String();
+		Date nuevaFecha=null;
+
 		do {
-		selector=menu();
+			selector = menu();
 			switch (selector) {
 			case 1: {
-				anadir();
+				nuevoTitulo= anadirTitulo();
+				contador = -1;
+				do {
+					if (titulo[contador] == null) {
+						titulo[contador] = nuevoTitulo;
+						encontrado = true;
+					}
+
+				} while (contador < titulo.length && !encontrado);
+				nuevaDescripcion= anadirDescripcion();
+				contador = -1;
+				do {
+					if (descripcion[contador] == null) {
+						descripcion[contador] = nuevaDescripcion;
+						encontrado = true;
+					}
+
+				} while (contador < titulo.length && !encontrado);
+			nuevaFecha= anadirFecha();
+			do {
+				if (fechaLimite[contador] == null) {
+					fechaLimite[contador] = nuevaFecha;
+					encontrado = true;
+				}
+
+			} while (contador < titulo.length && !encontrado);
 			}
 			default:
-				
+
 			}
-		}	
-		
-	
+		} while (selector != 5);
+
 	}
 
 	private static int menu() {
@@ -30,18 +59,28 @@ public class CRUDArray {
 		return selector;
 	}
 
-	private static void anadir() {
+	private static String anadirTitulo() {
 		System.out.println("====EVIL CORP====\n\n     AÑADIR\n     TAREA\n-----------------\n\n");
 		String nuevoTitulo = Utilidades.PedirString("Titulo de la tarea:");
-		String nuevaDescripcion = Utilidades.PedirString("Descripción de la tarea");
-		Date nuevaFecha = Utilidades.pedirFecha("Fecha límite:");
 		
-		for (int i = 0; i < titulo.length; i++) {
-	        if (titulo[i] == null) {
-	            titulo[i] = nuevoTitulo;
-	            break;
-	        }
+
+		return nuevoTitulo;
 	}
 
-}
+	private static String anadirDescripcion() {
+		System.out.println("====EVIL CORP====\n\n     AÑADIR\n     DESCRIPCIÓN\n-----------------\n\n");
+		String nuevaDescripcion = Utilidades.PedirString("Descripción de la tarea");
+
+		return nuevaDescripcion;
+	}
+	private static Date anadirFecha() {
+		System.out.println("====EVIL CORP====\n\n     AÑADIR\n     FECHA\n-----------------\n\n");
+		Date nuevaFecha = Utilidades.pedirFecha("Fecha límite:");
+		return nuevaFecha;
+	}
+	private void anadirTarea() {
+		
+		
+	}
+
 }
