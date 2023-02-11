5349a1ca2c0b0e4b4be6cb2f2b8797ca2425ed53
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-11 17:21:45 +0100

    Separados métodos en clase aparte (Main cojonudo)

--- CRUDArray.java
+++ CRUDArray.java
@@ -5,328 +5,39 @@ public class CRUDArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int selector = 0;
-		int contador = -1;
-		int posiciones = 2;
-		boolean encontrado = false;
+		int posiciones = 20;
 		String[] titulo = new String[posiciones];
 		String[] descripcion = new String[posiciones];
 		Date[] fechaLimite = new Date[posiciones];
-		String nuevoTitulo = new String();
-		String nuevaDescripcion = new String();
-		Date nuevaFecha = null;
-		char sino = 'n';
-		boolean ListaVacia = true;
-		boolean lleno = false;
-		String busqueda = new String();
-		boolean esnumero = false;
 
 		do {
-			selector = menu();
+			selector = MetodosCRUD.menu();
 			switch (selector) {
-			case 1: {
-				// Aqui se listan todas las tareas
-				System.out.println("\n\n LISTA DE\n TAREAS\n-----------------\n\n");
-				contador = 0;
-				ListaVacia = true;
-				while (contador < posiciones && titulo[0] != null) {
-					if (titulo[contador] != null) {
-						ListaVacia = false;
-						System.out.println(contador + ". " + titulo[contador] + " -- " + descripcion[contador] + " -- "
-								+ fechaLimite[contador]);
-					}
-					contador++;
-				}
-				if (ListaVacia) {
-					// si no hay tareas, se añade una
-					System.out.println("La lista está vacía");
-					sino = Utilidades.PedirChar("Añadir tarea? (S/N)");
-					if (sino == 's') {
-
-						nuevoTitulo = anadirTitulo();
-						titulo[contador] = nuevoTitulo;
-						nuevaDescripcion = anadirDescripcion();
-						descripcion[contador] = nuevaDescripcion;
-						nuevaFecha = anadirFecha();
-						fechaLimite[contador] = nuevaFecha;
-						System.out
-								.println("\n" + titulo[contador] + " ha sido añadido para el " + fechaLimite[contador]);
-					}
-
-				} else {
-					// aqui va un switch para modificar o borrar
-					selector = submenu();
-					switch (selector) {
-					case 1: {
-						busqueda = Utilidades.PedirString("¿Qué quieres modificar?");
-						esnumero = Utilidades.sonNumeros(busqueda);
-						if (esnumero) {
-							contador = Integer.parseInt(busqueda);
-							if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-								System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
-								nuevoTitulo = anadirTitulo();
-								titulo[contador] = nuevoTitulo;
-								nuevaDescripcion = anadirDescripcion();
-								descripcion[contador] = nuevaDescripcion;
-								nuevaFecha = anadirFecha();
-								fechaLimite[contador] = nuevaFecha;
-								System.out.println(
-										titulo[contador] + " ha sido modificado para el " + fechaLimite[contador]);
-							} else {
-								System.out.println("Lo siento, no encuentro lo que quieres modificar");
-								Utilidades.pausa();
-								break;
-							}
-						} else if (!esnumero) {
-							contador = buscarPosicionNombre(titulo, busqueda);
-							if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-								System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
-								nuevoTitulo = anadirTitulo();
-								titulo[contador] = nuevoTitulo;
-								nuevaDescripcion = anadirDescripcion();
-								descripcion[contador] = nuevaDescripcion;
-								nuevaFecha = anadirFecha();
-								fechaLimite[contador] = nuevaFecha;
-								System.out.println("\n" + titulo[contador] + " ha sido modificado para el "
-										+ fechaLimite[contador]);
-
-							} else {
-								System.out.println("No encuentro lo que quieres modificar");
-								Utilidades.pausa();
-								break;
-							}
-						}
-						Utilidades.pausa();
-						break;
-					}
-					case 2: {
-						busqueda = Utilidades.PedirString("\n¿Qué quieres borrar?");
-						esnumero = Utilidades.sonNumeros(busqueda);
-						if (esnumero) {
-							contador = Integer.parseInt(busqueda);
-							if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-								sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
-										+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
-								if (sino == 's') {
-
-									titulo[contador] = null;
-									descripcion[contador] = null;
-									fechaLimite[contador] = null;
-									System.out.println("\nTarea borrada con éxito");
-								} else {
-
-								}
-								Utilidades.pausa();
-								break;
-							} else {
-								System.out.println("No encuentro lo que quieres borrar");
-								Utilidades.pausa();
-								break;
-							}
-						} else if (!esnumero) {
-							contador = buscarPosicionNombre(titulo, busqueda);
-							if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-								sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
-										+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
-								if (sino == 's') {
-
-									titulo[contador] = null;
-									descripcion[contador] = null;
-									fechaLimite[contador] = null;
-									System.out.println("\nTarea borrada con éxito");
-									Utilidades.pausa();
-
-								} else {
-									Utilidades.pausa();
-									break;
-								}
-							} else {
-								Utilidades.pausa();
-								break;
-							}
-						}
-
-					}
-					default:
-						break;
-					}
+				case 1: {
+					MetodosCRUD.listar(posiciones, titulo, descripcion, fechaLimite);
+					break;
 				}
 
-				break;
-			}
-
-			case 2: {
-				MetodosCRUD.addTarea(posiciones, titulo, descripcion, fechaLimite);
-				break;
-			}
-			case 3: {
-				busqueda = Utilidades.PedirString("¿Qué quieres modificar?");
-				esnumero = Utilidades.sonNumeros(busqueda);
-				if (esnumero) {
-					contador = Integer.parseInt(busqueda);
-					if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-						System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
-						nuevoTitulo = anadirTitulo();
-						titulo[contador] = nuevoTitulo;
-						nuevaDescripcion = anadirDescripcion();
-						descripcion[contador] = nuevaDescripcion;
-						nuevaFecha = anadirFecha();
-						fechaLimite[contador] = nuevaFecha;
-						System.out.println(titulo[contador] + " ha sido modificado para el " + fechaLimite[contador]);
-					} else {
-						System.out.println("Lo siento, no encuentro lo que quieres modificar");
-						Utilidades.pausa();
-						break;
-					}
-				} else if (!esnumero) {
-					contador = buscarPosicionNombre(titulo, busqueda);
-					if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-						System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
-						nuevoTitulo = anadirTitulo();
-						titulo[contador] = nuevoTitulo;
-						nuevaDescripcion = anadirDescripcion();
-						descripcion[contador] = nuevaDescripcion;
-						nuevaFecha = anadirFecha();
-						fechaLimite[contador] = nuevaFecha;
-						System.out.println(
-								"\n" + titulo[contador] + " ha sido modificado para el " + fechaLimite[contador]);
-
-					} else {
-						System.out.println("No encuentro lo que quieres modificar");
-						Utilidades.pausa();
-						break;
-					}
+				case 2: {
+					MetodosCRUD.addTarea(posiciones, titulo, descripcion, fechaLimite);
+					break;
 				}
-			}
-			case 4: {
-				busqueda = Utilidades.PedirString("\n¿Qué quieres borrar?");
-				esnumero = Utilidades.sonNumeros(busqueda);
-				if (esnumero) {
-					contador = Integer.parseInt(busqueda);
-					if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-						sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
-								+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
-						if (sino == 's') {
-
-							titulo[contador] = null;
-							descripcion[contador] = null;
-							fechaLimite[contador] = null;
-							System.out.println("\nTarea borrada con éxito");
-						} else {
-
-						}
-						Utilidades.pausa();
-						break;
-					} else {
-						System.out.println("No encuentro lo que quieres borrar");
-						Utilidades.pausa();
-						break;
-					}
-				} else if (!esnumero) {
-					contador = buscarPosicionNombre(titulo, busqueda);
-					if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
-						sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
-								+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
-						if (sino == 's') {
-
-							titulo[contador] = null;
-							descripcion[contador] = null;
-							fechaLimite[contador] = null;
-							System.out.println("\nTarea borrada con éxito");
-							Utilidades.pausa();
-
-						} else {
-							Utilidades.pausa();
-							break;
-						}
-					} else {
-						Utilidades.pausa();
-						break;
-					}
+				case 3: {
+					MetodosCRUD.ModificarTarea(posiciones, titulo, descripcion, fechaLimite);
+					break;
 				}
-
-			}
-			case 5: {
-				busqueda = Utilidades.PedirString("\nBuscar Tarea:");
-				contador = buscarPosicionNombre(titulo, busqueda);
-				if (contador >= 0 && contador < posiciones) {
-					System.out.println(
-							titulo[contador] + " -- " + descripcion[contador] + " -- " + fechaLimite[contador]);
-
+				case 4: {
+					MetodosCRUD.eliminarTarea(posiciones, titulo, descripcion, fechaLimite);
 				}
-				Utilidades.pausa();
-				break;
-			}
-
-			default:
-				break;
+				case 5: {
+					MetodosCRUD.Buscar(posiciones, titulo, descripcion, fechaLimite);
+					Utilidades.pausa();
+					break;
+				}
+				default:
+					break;
 			}
 		} while (selector != 6);
-
-	}
-
-	/**
-	 * 
-	 * Muestra un menú con opciones para interactuar con una lista de tareas y
-	 * devuelve la opción seleccionada por el usuario.
-	 * 
-	 * @return int - La opción seleccionada por el usuario
-	 */
-	private static int menu() {
-
-		int selector = Utilidades.pedirInt(
-				"\n====EVIL CORP====\n   TAREAS 0.1    \n-----------------\n\n1. Lista Tareas\n2. Añadir Tarea\n3. Modificar Tarea\n4. Eliminar Tarea\n5. Buscar Tarea\n6. Salir\n-----------------\n\n   Selecciona \n   una opción\n\n-----------------");
-		return selector;
-	}
-
-	/**
-	 * 
-	 * Muestra un menú con las opciones de modificar una tarea, borrar una tarea o
-	 * salir. El usuario selecciona una opción y el método devuelve el número de la
-	 * opción seleccionada.
-	 * 
-	 * @return int La opción seleccionada por el usuario.
-	 */
-	private static int submenu() {
-
-		int selector = Utilidades.pedirInt(
-				"\n¿Qué quieres hacer?\n\n1. Modificar una tarea\n2. Borrar una tarea\n3. Atrás\n-----------------\n\n   Selecciona \n   una opción\n\n-----------------");
-		return selector;
-	}
-
-	
-
-
-	/**
-	 * 
-	 * Busca la posición en un array de un nombre específico.
-	 * 
-	 * @param lista  Es el array en el que se buscará la posición del nombre
-	 *               específico.
-	 * @param nombre Es el nombre que se buscará en el array.
-	 * @return La posición del nombre específico en el array, o -1 si no se
-	 *         encuentra.
-	 */
-	private static int buscarPosicionNombre(String[] lista, String nombre) {
-
-		int longitud = lista.length;
-		int i = 0;
-		int posicion = -1;
-		boolean encontrado = false;
-
-		if (longitud > 0) {
-			do {
-				if (lista[i] != null && lista[i].equals(nombre)) {
-					posicion = i;
-					encontrado = true;
-				}
-				i++;
-			} while (i < longitud && !encontrado);
-		}
-		if (!encontrado) {
-			System.out.println("Lo siento, no encuentro " + nombre + ".");
-		}
-
-		return posicion;
 
 	}
 
--- MetodosCRUD.java
+++ MetodosCRUD.java
@@ -2,39 +2,212 @@ import java.util.Date;
 
 public class MetodosCRUD {
 
-    public static void addTarea(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
-        int contador = 0;
-        boolean lleno = false;
-        boolean encontrado = false;
-        String nuevoTitulo;
-        String nuevaDescripcion;
-        Date nuevaFecha;
+	/**
+	 * Añade una tarea a los Arrays en la misma posición si queda espac io libre
+	 * @param posiciones el numero de posiciones que tienen los Arrays
+	 * @param titulo el titulo de la tarea
+	 * @param descripcion la descripción de la tarea
+	 * @param fechaLimite la fecha límite de la tarea
+	 */
+	public static void addTarea(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		int contador = 0;
+		boolean lleno = false;
+		boolean encontrado = false;
+		String nuevoTitulo;
+		String nuevaDescripcion;
+		Date nuevaFecha;
 
-        do {
-            if (titulo[contador] == null) {
-                encontrado = true;
-            } else {
-                contador++;
-                if (contador == posiciones) {
-                    lleno = true;
-                    errorLleno(posiciones);
-                    break;
-                }
-            }
-        } while (contador < posiciones && !encontrado);
-        if (encontrado && !lleno) {
-            nuevoTitulo = anadirTitulo();
-            titulo[contador] = nuevoTitulo;
-            nuevaDescripcion = anadirDescripcion();
-            descripcion[contador] = nuevaDescripcion;
-            nuevaFecha = anadirFecha();
-            fechaLimite[contador] = nuevaFecha;
-            System.out.println("\n" + titulo[contador] + " ha sido añadido para el " + fechaLimite[contador]);
-            Utilidades.pausa();
-        } else if (lleno) {
-            errorLleno(posiciones);
-        }
-    }
+		do {
+			if (titulo[contador] == null) {
+				encontrado = true;
+			} else {
+				contador++;
+				if (contador == posiciones) {
+					lleno = true;
+					errorLleno(posiciones);
+					break;
+				}
+			}
+		} while (contador < posiciones && !encontrado);
+		if (encontrado && !lleno) {
+			nuevoTitulo = anadirTitulo();
+			titulo[contador] = nuevoTitulo;
+			nuevaDescripcion = anadirDescripcion();
+			descripcion[contador] = nuevaDescripcion;
+			nuevaFecha = anadirFecha();
+			fechaLimite[contador] = nuevaFecha;
+			System.out.println("\n" + titulo[contador] + " ha sido añadido para el " + fechaLimite[contador]);
+			Utilidades.pausa();
+		} else if (lleno) {
+			errorLleno(posiciones);
+		}
+	}
+
+	/**
+	 * Lista las tareas y permite modificarlas o eliminarlas
+	 * @param posiciones
+	 * @param titulo
+	 * @param descripcion
+	 * @param fechaLimite
+	 */
+	public static void listar(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		int contador = 0;
+		String nuevoTitulo;
+		String nuevaDescripcion;
+		Date nuevaFecha;
+		boolean ListaVacia;
+		char sino;
+		int selector;
+
+		// Aqui se listan todas las tareas
+		System.out.println("\n\n LISTA DE\n TAREAS\n-----------------\n\n");
+		contador = 0;
+		ListaVacia = true;
+		while (contador < posiciones && titulo[0] != null) {
+			if (titulo[contador] != null) {
+				ListaVacia = false;
+				System.out.println(contador + ". " + titulo[contador] + " -- " + descripcion[contador] + " -- "
+						+ fechaLimite[contador]);
+			}
+			contador++;
+		}
+		if (ListaVacia) {
+			// si no hay tareas, se añade una
+			System.out.println("La lista está vacía");
+			sino = Utilidades.PedirChar("Añadir tarea? (S/N)");
+			if (sino == 's') {
+
+				nuevoTitulo = anadirTitulo();
+				titulo[contador] = nuevoTitulo;
+				nuevaDescripcion = anadirDescripcion();
+				descripcion[contador] = nuevaDescripcion;
+				nuevaFecha = anadirFecha();
+				fechaLimite[contador] = nuevaFecha;
+				System.out
+						.println("\n" + titulo[contador] + " ha sido añadido para el " + fechaLimite[contador]);
+			}
+
+		} else {
+			// aqui va un switch para modificar o borrar
+			selector = submenu();
+			switch (selector) {
+				case 1: {
+					ModificarTarea(posiciones, titulo, descripcion, fechaLimite);
+					break;
+				}
+				case 2: {
+					eliminarTarea(posiciones, titulo, descripcion, fechaLimite);
+					break;
+				}
+				default:
+					break;
+			}
+		}
+	}
+
+	public static void ModificarTarea(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		String busqueda;
+		boolean esnumero;
+		int contador = -1;
+		String nuevoTitulo;
+		String nuevaDescripcion;
+		Date nuevaFecha;
+
+		busqueda = Utilidades.PedirString("¿Qué quieres modificar?");
+		esnumero = Utilidades.sonNumeros(busqueda);
+		if (esnumero) {
+			contador = Integer.parseInt(busqueda);
+			if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
+				System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
+				nuevoTitulo = anadirTitulo();
+				titulo[contador] = nuevoTitulo;
+				nuevaDescripcion = anadirDescripcion();
+				descripcion[contador] = nuevaDescripcion;
+				nuevaFecha = anadirFecha();
+				fechaLimite[contador] = nuevaFecha;
+				System.out.println(titulo[contador] + " ha sido modificado para el " + fechaLimite[contador]);
+			} else {
+				System.out.println("Lo siento, no encuentro lo que quieres modificar");
+				Utilidades.pausa();
+			}
+		} else if (!esnumero) {
+			contador = buscarPosicionNombre(titulo, busqueda);
+			if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
+				System.out.println("Modificando " + titulo[contador] + " del " + fechaLimite[contador]);
+				nuevoTitulo = anadirTitulo();
+				titulo[contador] = nuevoTitulo;
+				nuevaDescripcion = anadirDescripcion();
+				descripcion[contador] = nuevaDescripcion;
+				nuevaFecha = anadirFecha();
+				fechaLimite[contador] = nuevaFecha;
+				System.out.println(
+						"\n" + titulo[contador] + " ha sido modificado para el " + fechaLimite[contador]);
+
+			} else {
+				System.out.println("No encuentro lo que quieres modificar");
+				Utilidades.pausa();
+			}
+		}
+	}
+
+	public static void eliminarTarea(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		String busqueda;
+		boolean esnumero;
+		int contador = -1;
+		char sino;
+
+		busqueda = Utilidades.PedirString("\n¿Qué quieres borrar?");
+		esnumero = Utilidades.sonNumeros(busqueda);
+		if (esnumero) {
+			contador = Integer.parseInt(busqueda);
+			if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
+				sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
+						+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
+				if (sino == 's') {
+
+					titulo[contador] = null;
+					descripcion[contador] = null;
+					fechaLimite[contador] = null;
+					System.out.println("\nTarea borrada con éxito");
+				} else {
+
+				}
+				Utilidades.pausa();
+			} else {
+				System.out.println("No encuentro lo que quieres borrar");
+				Utilidades.pausa();
+			}
+		} else if (!esnumero) {
+			contador = buscarPosicionNombre(titulo, busqueda);
+			if (contador >= 0 && contador < posiciones && titulo[contador] != null) {
+				sino = Utilidades.PedirChar("\n¿Seguro que quieres borrar " + titulo[contador] + " del "
+						+ fechaLimite[contador] + "? (S/N)\nEste cambio no se podrá deshacer");
+				if (sino == 's') {
+
+					titulo[contador] = null;
+					descripcion[contador] = null;
+					fechaLimite[contador] = null;
+					System.out.println("\nTarea borrada con éxito");
+					Utilidades.pausa();
+
+				} else {
+					Utilidades.pausa();
+				}
+			}
+		}
+
+	}
+
+	public static void Buscar(int posiciones, String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		int contador = -1;
+		String busqueda = Utilidades.PedirString("\nBuscar Tarea:");
+		contador = buscarPosicionNombre(titulo, busqueda);
+		if (contador >= 0 && contador < posiciones) {
+			System.out.println(
+					titulo[contador] + " -- " + descripcion[contador] + " -- " + fechaLimite[contador]);
+
+		}
+	}
 
 	/**
 	 * Muestra un mensaje de error en pantalla indicando que la memoria está llena y
@@ -50,7 +223,7 @@ public class MetodosCRUD {
 
 	}
 
-    /**
+	/**
 	 * 
 	 * Muestra un mensaje de bienvenida y solicita al usuario que introduzca el
 	 * título de la tarea a añadir.
@@ -90,4 +263,68 @@ public class MetodosCRUD {
 		Date nuevaFecha = Utilidades.pedirFecha("Fecha límite:");
 		return nuevaFecha;
 	}
+
+	/**
+	 * 
+	 * Busca la posición en un array de un nombre específico.
+	 * 
+	 * @param lista  Es el array en el que se buscará la posición del nombre
+	 *               específico.
+	 * @param nombre Es el nombre que se buscará en el array.
+	 * @return La posición del nombre específico en el array, o -1 si no se
+	 *         encuentra.
+	 */
+	private static int buscarPosicionNombre(String[] lista, String nombre) {
+
+		int longitud = lista.length;
+		int i = 0;
+		int posicion = -1;
+		boolean encontrado = false;
+
+		if (longitud > 0) {
+			do {
+				if (lista[i] != null && lista[i].equals(nombre)) {
+					posicion = i;
+					encontrado = true;
+				}
+				i++;
+			} while (i < longitud && !encontrado);
+		}
+		if (!encontrado) {
+			System.out.println("Lo siento, no encuentro " + nombre + ".");
+		}
+
+		return posicion;
+
+	}
+
+	/**
+	 * 
+	 * Muestra un menú con opciones para interactuar con una lista de tareas y
+	 * devuelve la opción seleccionada por el usuario.
+	 * 
+	 * @return int - La opción seleccionada por el usuario
+	 */
+	public static int menu() {
+
+		int selector = Utilidades.pedirInt(
+				"\n====EVIL CORP====\n   TAREAS 0.1    \n-----------------\n\n1. Lista Tareas\n2. Añadir Tarea\n3. Modificar Tarea\n4. Eliminar Tarea\n5. Buscar Tarea\n6. Salir\n-----------------\n\n   Selecciona \n   una opción\n\n-----------------");
+		return selector;
+	}
+
+	/**
+	 * 
+	 * Muestra un menú con las opciones de modificar una tarea, borrar una tarea o
+	 * salir. El usuario selecciona una opción y el método devuelve el número de la
+	 * opción seleccionada.
+	 * 
+	 * @return int La opción seleccionada por el usuario.
+	 */
+	private static int submenu() {
+
+		int selector = Utilidades.pedirInt(
+				"\n¿Qué quieres hacer?\n\n1. Modificar una tarea\n2. Borrar una tarea\n3. Atrás\n-----------------\n\n   Selecciona \n   una opción\n\n-----------------");
+		return selector;
+	}
+
 }
