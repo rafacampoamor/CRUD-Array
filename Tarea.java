78607e6be70a9a7e6760156856e8d8dc94d3e8bf
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-19 13:54:29 +0100

    Revert "descargar"
    This reverts commit 7a8fd7f2efcd874342914a910cb720420836ab9e.

---
+++ Tarea.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Date;
+
+public class Tarea {
+	public Tarea(String[] titulo, String[] descripcion, Date[] fechaLimite) {
+		super();
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.fechaLimite = fechaLimite;
+	}
+	public Tarea() {
+		super();
+	}
+	private int posiciones = 20;
+	private String[] titulo = new String[posiciones];
+	private String[] descripcion = new String[posiciones];
+	private Date[] fechaLimite = new Date[posiciones];
+	
+	public String[] getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(int posicion, String titulo) {
+		//para hacer funcionar este setter, he tirado de Google
+		this.titulo[posicion] = titulo;
+	}
+	public String[] getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(int posicion, String descripcion) {
+		//para hacer funcionar este setter, he tirado de Google
+		this.descripcion[posicion] = descripcion;
+	}
+	public Date[] getFechaLimite() {
+		return fechaLimite;
+	}
+	public void setFechaLimite(int posicion, Date fechaLimite) {
+		//para hacer funcionar este setter, he tirado de Google
+		this.fechaLimite[posicion] = fechaLimite;
+	}
+	
+	public int getPosiciones() {
+		return posiciones;
+	}
+	public void setPosiciones(int posiciones) {
+		this.posiciones = posiciones;
+	}
+	@Override
+	public String toString() {
+		return "tarea [titulo=" + Arrays.toString(titulo) + ", descripcion=" + Arrays.toString(descripcion)
+				+ ", fechaLimite=" + Arrays.toString(fechaLimite) + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+}
